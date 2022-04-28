056f2794cb2baced48d5d056893cb70e77db0830
hari-govind <39461016+hari-govind@users.noreply.github.com>
2022-04-28 22:54:09 +0530

    Return ErrBucketAlreadyExists if status code is 409

--- api/pkg/s3/service.go
+++ api/pkg/s3/service.go
@@ -22,6 +22,7 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"regexp"
 
 	"github.com/micro/go-micro/v2/client"
 
@@ -153,6 +154,12 @@ func (s *APIService) isBackendExist(ctx context.Context, backendName string) boo
 
 func HandleS3Error(response *restful.Response, request *restful.Request, err error, errCode int32) error {
 	if err != nil {
+		isStatus409, er := regexp.Match("status\\scode:\\s409", []byte(err.Error()))
+                if er == nil {
+                        if isStatus409 {
+				err =  s3error.ErrBucketAlreadyExists
+                        }
+                }
 		WriteErrorResponse(response, request, err)
 		return err
 	}
